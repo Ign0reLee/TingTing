edf09d5d122150ba70d427056cabdbc18e3fc364
Ign0reLee <dlwpdud@gmail.com>
2020-08-13 00:46:42 +0900

    module update

--- FacialLandMark/__init__.py
+++ FacialLandMark/__init__.py
@@ -1,2 +1,8 @@
-from utils import *
-from tingting import *
+# -*- coding: utf-8 -*-
+
+__author__ ="LeeJeYoung"
+__email__ = "dlwpdud@gmail.com"
+__version__ = "0.1.0"
+
+from .utils import *
+from .tingting import *
